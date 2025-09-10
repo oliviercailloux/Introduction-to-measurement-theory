7cf8a7a5bdf7551587e036908334efe2d4e8aa9d
Olivier Cailloux <olivier.cailloux@gmail.com>
2025-09-11 00:01:44 +0200

    to fo

--- Build/src/main/java/io/github/oliviercailloux/build/Builder.java
+++ Build/src/main/java/io/github/oliviercailloux/build/Builder.java
@@ -5,12 +5,14 @@ import com.google.common.collect.ImmutableSet;
 import com.google.common.io.CharSource;
 import com.google.common.io.MoreFiles;
 import io.github.oliviercailloux.docbook.DocBookResources;
+import io.github.oliviercailloux.docbook.xslt1.DocBookXslt1Resources;
 import io.github.oliviercailloux.docbook.xslt3.DocBookXslt3Resources;
 import io.github.oliviercailloux.jaris.xml.ConformityChecker;
 import io.github.oliviercailloux.jaris.xml.KnownFactory;
 import io.github.oliviercailloux.jaris.xml.XmlTransformer;
 import io.github.oliviercailloux.jaris.xml.XmlTransformerFactory;
 import io.github.oliviercailloux.publish.DocBookConformityChecker;
+import io.github.oliviercailloux.publish.FoToPdfTransformer;
 import java.io.IOException;
 import java.net.URI;
 import java.nio.file.Files;
@@ -25,12 +27,13 @@ public class Builder implements AutoCloseable {
   @SuppressWarnings("unused")
   private static final Logger LOGGER = LoggerFactory.getLogger(Builder.class);
 
-  private static final Path INPUT_DIR = Path.of("");
-  private static final Path OUTPUT_DIR = Path.of("../Online Pages/");
+  private static final Path INPUT_DIR = Path.of("..");
+  private static final Path OUTPUT_DIR = Path.of("../../Online Pages/");
   // private static final CharSource STYLE = charSource("MyStyle.xsl");
-  private static final URI STYLE = DocBookXslt3Resources.XSLT_3_HTML_URI;
+  private static final URI TO_FO_STYLE = DocBookXslt1Resources.XSLT_1_FO_URI;
+  private static final URI TO_HTML_STYLE = DocBookXslt3Resources.XSLT_3_HTML_URI;
 
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) throws Exception {
     LOGGER.debug("CWD: {}.", Path.of(".").toAbsolutePath().normalize());
     try (Builder builder = new Builder()) {
       LOGGER.info("Clearing.");
@@ -56,7 +59,9 @@ public class Builder implements AutoCloseable {
   private final Asciidoctor asciidoctor;
   private final Options options;
   private final ConformityChecker docBookChecker;
-  private final XmlTransformer transformer;
+  private final XmlTransformer toFo;
+  private final FoToPdfTransformer toPdf;
+  private final XmlTransformer toHtml;
 
   private Builder() {
     asciidoctor = Asciidoctor.Factory.create();
@@ -70,7 +75,9 @@ public class Builder implements AutoCloseable {
       throw new VerifyException(e);
     }
     underlying.setURIResolver(DocBookResources.XML_RESOLVER.getURIResolver());
-    transformer = XmlTransformerFactory.usingFactory(underlying).usingStylesheet(STYLE);
+    toFo = XmlTransformerFactory.usingFactory(underlying).usingStylesheet(TO_FO_STYLE);
+    toPdf = FoToPdfTransformer.usingFactory(underlying);
+    toHtml = XmlTransformerFactory.usingFactory(underlying).usingStylesheet(TO_HTML_STYLE);
   }
 
   public void convert(String name) throws IOException {
@@ -86,8 +93,16 @@ public class Builder implements AutoCloseable {
     docBookChecker.verifyValid(CharSource.wrap(docBook));
     // Files.writeString(OUTPUT_DIR.resolve("out.dbk"), docBook);
 
+    LOGGER.info("Transforming DocBook to FO.");
+    final String fo = toFo.charsToChars(docBook);
+    // Files.writeString(OUTPUT_DIR.resolve("%s.fo".formatted(name)), fo);
+
+    // LOGGER.info("Transforming FO to PDF.");
+    // final byte[] pdf = toPdf.charsToBytes(fo);
+    // Files.write(OUTPUT_DIR.resolve("%s.pdf".formatted(name)), pdf);
+    
     LOGGER.info("Transforming DocBook to HTML.");
-    final String html = transformer.charsToChars(docBook);
+    final String html = toHtml.charsToChars(docBook);
     Files.writeString(OUTPUT_DIR.resolve("%s.html".formatted(name)), html);
   }
 
